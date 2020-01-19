0654c826c1f65a1c85510386d49f24dbb2f22ec8
BarryFoye <barry.foye@gmail.com>
2020-01-19 23:36:50 +0000

    added some ui and variable behaviour for the players; corrected paddle bug (not well tested)

--- server/Resume/web_app/public/search_sketch.js
+++ server/Resume/web_app/public/search_sketch.js
@@ -8,12 +8,17 @@ let paddle_down_right = true;
 let paddle_down_left = false;
 let circle_x;
 let circle_y;
+let paddle_speed_right;
+let paddle_speed_left;
 let x_speed;
 let y_speed;
 let coin_flip;
 let player_1 = { "score": 0, "streak": 0 };
 let player_2 = { "score": 0, "streak": 0 };
-let top_streak = 0;
+let top_streak = { "player": "none", "streak": 0 };
+
+let hit_left = false;
+let hit_right = false;
 
 function setup() {
     let canvas = createCanvas(400, 400);
@@ -27,11 +32,12 @@ function setup() {
     coin_flip = floor(random(2));
     x_speed = floor(random(1, 3));
     y_speed = floor(random(1, 3));
+    paddle_speed_right = floor(random(2, 5));
+    paddle_speed_left = floor(random(2, 5));
     if (coin_flip > 0) {
         x_speed *= -1;
         y_speed *= -1;
     }
-    //frameRate(4);
 }
 
 function draw() {
@@ -49,21 +55,29 @@ function draw() {
 
 function updateScores() {
     document.getElementById("player1").innerHTML = "Player 1<br><strong>" + player_1.score + "</strong>";
-
     document.getElementById("player2").innerHTML = "Player 2<br><strong>" + player_2.score + "</strong>";
+    document.getElementById("streak").innerHTML = "Highest Streak<br><strong>" + top_streak.streak + "</strong>";
 }
 
 function hitCheck() {
-    // check left paddle
-    if (circle_x - 5 <= paddle_left_x + paddle_width
-        && circle_y - 5 > paddle_left_y
-        && circle_y + 5 < paddle_left_y + paddle_height) {
-        x_speed = -x_speed;
-    }
-    if (circle_x + 5 >= paddle_right_x
-        && circle_y - 5 > paddle_right_y
-        && circle_y + 5 < paddle_right_y + paddle_height) {
-        x_speed = -x_speed;
+    if (circle_x - 5 < paddle_left_x || circle_x + 5 > paddle_right_x + paddle_width) {
+
+    } else {
+        // check left paddle
+        if (!hit_left && circle_x - 5 <= paddle_left_x + paddle_width
+            && circle_y - 5 > paddle_left_y
+            && circle_y + 5 < paddle_left_y + paddle_height) {
+            x_speed = -x_speed;
+            hit_left = true;
+            hit_right = false;
+        }
+        if (!hit_right && circle_x + 5 >= paddle_right_x
+            && circle_y - 5 > paddle_right_y
+            && circle_y + 5 < paddle_right_y + paddle_height) {
+            x_speed = -x_speed;
+            hit_left = false;
+            hit_right = true;
+        }
     }
 }
 
@@ -79,10 +93,10 @@ function drawRectRight() {
     }
 
     if (paddle_down_right) {
-        paddle_right_y += 2;
+        paddle_right_y += paddle_speed_right;
     }
     else if (!paddle_down_right) {
-        paddle_right_y -= 2;
+        paddle_right_y -= paddle_speed_right;
     }
 }
 
@@ -98,10 +112,10 @@ function drawRectLeft() {
     }
 
     if (paddle_down_left) {
-        paddle_left_y += 2;
+        paddle_left_y += paddle_speed_left;
     }
     else if (!paddle_down_left) {
-        paddle_left_y -= 2;
+        paddle_left_y -= paddle_speed_left;
     }
 }
 
@@ -115,13 +129,20 @@ function drawCircle() {
         player_1.score++;
         player_1.streak++;
         player_2.streak = 0;
-        if (top_streak < player_1.streak) top_streak = player_1.streak;
+        if (top_streak.streak < player_1.streak) {
+            top_streak.player = "Player 1";
+            top_streak.streak = player_1.streak;
+        }
+
         reset();
     } else if (circle_x < - 10) {
         player_2.score++;
         player_2.streak++;
         player_1.streak = 0;
-        if (top_streak < player_2.streak) top_streak = player_2.streak;
+        if (top_streak < player_2.streak) {
+            top_streak.player = "Player 2";
+            top_streak.streak = player_2.streak;
+        }
         reset();
     }
     if (circle_y > height - 5 || circle_y < 5) {
@@ -139,4 +160,10 @@ function reset() {
         x_speed *= -1;
         y_speed *= -1;
     }
+
+    hit_left = false;
+    hit_right = false;
+
+    paddle_speed_right = floor(random(2, 5));
+    paddle_speed_left = floor(random(2, 5));
 }
