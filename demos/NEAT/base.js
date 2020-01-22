07787a8f960d834d7effbb80ea7239bf36e07fe5
BarryFoye <barry.foye@gmail.com>
2020-01-22 23:17:03 +0000

    added uniform crossover

--- demos/NEAT/base.js
+++ demos/NEAT/base.js
@@ -1,38 +1,68 @@
 //https://en.wikipedia.org/wiki/Stochastic_universal_sampling
 //https://www.tutorialspoint.com/genetic_algorithms/genetic_algorithms_parent_selection.htm
 
-let population;
-const optimum = "0000000000"
+let oldPopulation;
+let newPopulation;
+const optimum = "111111101010"
 
 function initialise() {
-    population = [
-        { "id": 1, "solution": "1100101010", "fitness": 0 },
-        { "id": 2, "solution": "1000101010", "fitness": 0 },
-        { "id": 3, "solution": "1100111010", "fitness": 0 },
-        { "id": 4, "solution": "1100101011", "fitness": 0 },
-        { "id": 5, "solution": "1100011010", "fitness": 0 },
-        { "id": 6, "solution": "1010101010", "fitness": 0 },
-        { "id": 7, "solution": "1111100000", "fitness": 0 },
-        { "id": 8, "solution": "0000011111", "fitness": 0 },
-        { "id": 9, "solution": "0011101010", "fitness": 0 },
-        { "id": 10, "solution": "0100101011", "fitness": 0 }
+    oldPopulation = [
+        { "solution": "1100101010", "fitness": 0 },
+        { "solution": "1000101010", "fitness": 0 },
+        { "solution": "1100111010", "fitness": 0 },
+        { "solution": "1100101011", "fitness": 0 },
+        { "solution": "1100011010", "fitness": 0 },
+        { "solution": "1010101010", "fitness": 0 },
+        { "solution": "1111100000", "fitness": 0 },
+        { "solution": "0000011111", "fitness": 0 },
+        { "solution": "0011101010", "fitness": 0 },
+        { "solution": "0100101011", "fitness": 0 }
     ];
+    newPopulation = [];
 }
 initialise();
 
 
 function selection() {
-    for (let i = 0; i < population.length; i++) {
-        population[i].fitness = evaluate(population[i]);
+    for (let i = 0; i < oldPopulation.length; i++) {
+        oldPopulation[i].fitness = evaluate(oldPopulation[i]);
     }
 }
 selection();
-console.log(selectPop(population, 7));
-
+let candidates = selectPop(oldPopulation, 6);
 function crossOver() {
+    let parent_a = "";
+    let parent_b = "";
+    let child_a = "";
+    let child_b = "";
+    let newPop = []
 
+    for (let i = 0; i < candidates.length - 1; i++) {
+        parent_a = candidates[i].solution;
+        parent_b = candidates[i + 1].solution;
+        for (let j = 0; j < parent_a.length; j++) {
+            if (Math.random() > 0.5) {
+                child_a += parent_a[j];
+                child_b += parent_b[j];
+            } else {
+                child_b += parent_a[j];
+                child_a += parent_b[j];
+            }
+        }
+        newPop.push({ "solution": child_a, "fitness": 0 })
+        newPop.push({ "solution": child_b, "fitness": 0 });
+        child_a = "";
+        child_b = "";
+    }
+    newPop.push(candidates[candidates.length - 1]);
+    return newPop;
 }
-
+newPopulation = crossOver();
+console.log("**************oldPopulation**************");
+console.log(oldPopulation);
+console.log();
+console.log("**************newPopulation**************");
+console.log(newPopulation);
 function mutate() {
 
 }
@@ -57,23 +87,23 @@ function selectPop(pop, offspring) {
         sum_pop_fitness += pop[i].fitness;
     }
     let distance_between_pointers = Math.floor(sum_pop_fitness / num_offspring);
-    let start = getRandomInt(0, distance_between_pointers);
-    let pointers = [];  
+    // From the aboe resource, now not used: let start = getRandomInt(0, distance_between_pointers);
+    let pointers = [];
     for (let i = 0; i < num_offspring; i++) {
-        pointers.push(getRandomInt(0, sum_pop_fitness -1));
+        pointers.push(getRandomInt(0, sum_pop_fitness - 1));
     }
     pointers.sort((a, b) => (a > b) ? 1 : -1);
-    pop.sort((a,b) => (a.fitness > b.fitness) ? 1 : -1);
+    pop.sort((a, b) => (a.fitness > b.fitness) ? 1 : -1);
     let keep = [];
-    
-    for (let j = 0; j < pointers.length; j++){
+
+    for (let j = 0; j < pointers.length; j++) {
         let i = 0;
         let fitness_sum = 0;
         let p = pointers[j];
-        while (fitness_sum < p){
+        while (fitness_sum < p) {
             fitness_sum += pop[i].fitness;
-            i++; 
-            if(i === pop.length) i--;           
+            i++;
+            if (i === pop.length) i--;
         }
         keep.push(pop[i]);
     }
@@ -86,3 +116,5 @@ function getRandomInt(min, max) {
     max = Math.floor(max);
     return Math.floor(Math.random() * (max - min)) + min; //The maximum is exclusive and the minimum is inclusive
 }
+
+
